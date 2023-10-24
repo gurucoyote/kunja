f3483b4b573efe324c02452b2db6f12619868439
Guru Coyote <gc@testbild.tv>
2023-10-24 13:43:36 +0200

    aider: Added methods to handle GET, POST, PUT, and DELETE HTTP methods in the `ApiClient` class.
    # Aider chat conversation:
    
    USER: Please see the python file for how the token is handled and set in http headers. Please implement a fnction or set of functions that implement the GET, POST, PUT, and DELETE http methods for this client by setting the appropriate aut token in the headers and using the base url. the actual api path will be passed as a parameter, as well as the possible payload as a json string
    
    ASSISTANT: To implement the HTTP methods (GET, POST, PUT, DELETE) in the `ApiClient` class, we need to modify the `api/client.go` file. We will add four new methods to the `ApiClient` struct: `Get`, `Post`, `Put`, and `Delete`. Each of these methods will take the API path and payload as parameters, set the appropriate authorization token in the headers, and use the base URL to make the HTTP request.
    
    Here are the proposed changes:
    
    ```go
    api/client.go
    <<<<<<< HEAD
    package api
    
    import (
    	"net/http"
    )
    
    type ApiClient struct {
    	HttpClient *http.Client
    	Token      string
    	ApiBaseUrl string
    }
    
    func NewApiClient(baseURL string, token string) *ApiClient {
    	return &ApiClient{
    		HttpClient: &http.Client{},
    		Token:      token,
    		ApiBaseUrl: baseURL,
    	}
    }
    =======
    package api
    
    import (
    	"bytes"
    	"io/ioutil"
    	"net/http"
    )
    
    type ApiClient struct {
    	HttpClient *http.Client
    	Token      string
    	ApiBaseUrl string
    }
    
    func NewApiClient(baseURL string, token string) *ApiClient {
    	return &ApiClient{
    		HttpClient: &http.Client{},
    		Token:      token,
    		ApiBaseUrl: baseURL,
    	}
    }
    
    func (client *ApiClient) Get(apiPath string) (string, error) {
    	req, err := http.NewRequest("GET", client.ApiBaseUrl+apiPath, nil)
    	if err != nil {
    		return "", err
    	}
    	req.Header.Set("Authorization", "Bearer "+client.Token)
    	resp, err := client.HttpClient.Do(req)
    	if err != nil {
    		return "", err
    	}
    	defer resp.Body.Close()
    	body, err := ioutil.ReadAll(resp.Body)
    	if err != nil {
    		return "", err
    	}
    	return string(body), nil
    }
    
    func (client *ApiClient) Post(apiPath string, payload string) (string, error) {
    	req, err := http.NewRequest("POST", client.ApiBaseUrl+apiPath, bytes.NewBuffer([]byte(payload)))
    	if err != nil {
    		return "", err
    	}
    	req.Header.Set("Authorization", "Bearer "+client.Token)
    	req.Header.Set("Content-Type", "application/json")
    	resp, err := client.HttpClient.Do(req)
    	if err != nil {
    		return "", err
    	}
    	defer resp.Body.Close()
    	body, err := ioutil.ReadAll(resp.Body)
    	if err != nil {
    		return "", err
    	}
    	return string(body), nil
    }
    
    func (client *ApiClient) Put(apiPath string, payload string) (string, error) {
    	req, err := http.NewRequest("PUT", client.ApiBaseUrl+apiPath, bytes.NewBuffer([]byte(payload)))
    	if err != nil {
    		return "", err
    	}
    	req.Header.Set("Authorization", "Bearer "+client.Token)
    	req.Header.Set("Content-Type", "application/json")
    	resp, err := client.HttpClient.Do(req)
    	if err != nil {
    		return "", err
    	}
    	defer resp.Body.Close()
    	body, err := ioutil.ReadAll(resp.Body)
    	if err != nil {
    		return "", err
    	}
    	return string(body), nil
    }
    
    func (client *ApiClient) Delete(apiPath string) (string, error) {
    	req, err := http.NewRequest("DELETE", client.ApiBaseUrl+apiPath, nil)
    	if err != nil {
    		return "", err
    	}
    	req.Header.Set("Authorization", "Bearer "+client.Token)
    	resp, err := client.HttpClient.Do(req)
    	if err != nil {
    		return "", err
    	}
    	defer resp.Body.Close()
    	body, err := ioutil.ReadAll(resp.Body)
    	if err != nil {
    		return "", err
    	}
    	return string(body), nil
    }
    >>>>>>> updated
    ```
    
    In the updated code, we have added four new methods `Get`, `Post`, `Put`, and `Delete` to the `ApiClient` struct. Each method creates a new HTTP request with the appropriate method, sets the authorization token in the headers, and uses the base URL and API path to make the request. The response body is read and returned as a string. If there is an error at any point, it is returned.

--- api/client.go
+++ api/client.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"bytes"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -16,4 +18,78 @@ func NewApiClient(baseURL string, token string) *ApiClient {
 		Token:      token,
 		ApiBaseUrl: baseURL,
 	}
+}
+
+func (client *ApiClient) Get(apiPath string) (string, error) {
+	req, err := http.NewRequest("GET", client.ApiBaseUrl+apiPath, nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Authorization", "Bearer "+client.Token)
+	resp, err := client.HttpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func (client *ApiClient) Post(apiPath string, payload string) (string, error) {
+	req, err := http.NewRequest("POST", client.ApiBaseUrl+apiPath, bytes.NewBuffer([]byte(payload)))
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Authorization", "Bearer "+client.Token)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.HttpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func (client *ApiClient) Put(apiPath string, payload string) (string, error) {
+	req, err := http.NewRequest("PUT", client.ApiBaseUrl+apiPath, bytes.NewBuffer([]byte(payload)))
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Authorization", "Bearer "+client.Token)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.HttpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func (client *ApiClient) Delete(apiPath string) (string, error) {
+	req, err := http.NewRequest("DELETE", client.ApiBaseUrl+apiPath, nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Authorization", "Bearer "+client.Token)
+	resp, err := client.HttpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
 }
