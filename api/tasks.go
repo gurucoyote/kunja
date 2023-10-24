9bfcbdb9fe4a89f6b3fa17ccb61285a6be4f84e0
Guru Coyote <gc@testbild.tv>
2023-10-24 20:40:17 +0200

    aider: Implemented urgency calculation for tasks in Go.
    # Aider chat conversation:
    
    USER: please implement the go equivalent of the urgency calculation from the python code in the tasks.go file. Use only the default values from the python code, do not load any config. Also ommit the keyword based calculation for projects or labels
    
    ASSISTANT: To implement the urgency calculation in Go, we need to modify the `api/tasks.go` file. We will add a new method `CalculateUrgency` to the `Task` struct. This method will implement the same logic as the `compute_for` method in the Python code.
    
    Here are the changes:
    
    ```go
    api/tasks.go
    <<<<<<< HEAD
    package api
    
    import (
    	"encoding/json"
    	"github.com/google/go-querystring/query"
    )
    
    func (client *ApiClient) GetAllTasks(params GetAllTasksParams) ([]Task, error) {
    	queryParams, _ := query.Values(params)
    	response, err := client.Get("/tasks/all?" + queryParams.Encode())
    	if err != nil {
    		return nil, err
    	}
    	var tasks []Task
    	err = json.Unmarshal([]byte(response), &tasks)
    	if err != nil {
    		return nil, err
    	}
    	return tasks, nil
    }
    =======
    package api
    
    import (
    	"encoding/json"
    	"github.com/google/go-querystring/query"
    	"time"
    )
    
    func (client *ApiClient) GetAllTasks(params GetAllTasksParams) ([]Task, error) {
    	queryParams, _ := query.Values(params)
    	response, err := client.Get("/tasks/all?" + queryParams.Encode())
    	if err != nil {
    		return nil, err
    	}
    	var tasks []Task
    	err = json.Unmarshal([]byte(response), &tasks)
    	if err != nil {
    		return nil, err
    	}
    	for i := range tasks {
    		tasks[i].CalculateUrgency()
    	}
    	return tasks, nil
    }
    
    func (task *Task) CalculateUrgency() {
    	if task.Done {
    		task.Urgency = 0
    		return
    	}
    	dueDateScore := task.getDueDateScore()
    	priorityScore := task.Priority
    	favoriteScore := 0
    	if task.IsFavorite {
    		favoriteScore = 1
    	}
    	task.Urgency = 1 + dueDateScore + priorityScore + favoriteScore
    }
    
    func (task *Task) getDueDateScore() int {
    	if task.DueDate == nil {
    		return 0
    	}
    	dueDays := int(task.DueDate.Sub(time.Now()).Hours() / 24)
    	switch {
    	case dueDays < 0:
    		return 6
    	case dueDays == 0:
    		return 5
    	case dueDays == 1:
    		return 4
    	case dueDays > 1 && dueDays <= 2:
    		return 3
    	case dueDays > 2 && dueDays <= 5:
    		return 2
    	case dueDays > 5 && dueDays <= 10:
    		return 1
    	case dueDays > 14:
    		return -1
    	default:
    		return 0
    	}
    }
    >>>>>>> updated
    ```
    
    In the updated code, we added the `CalculateUrgency` method to the `Task` struct. This method calculates the urgency score based on the task's due date, priority, and favorite status. The `getDueDateScore` method calculates the due date score based on the number of days until the task's due date. We also modified the `GetAllTasks` method to calculate the urgency score for each task after retrieving them from the API.

--- api/tasks.go
+++ api/tasks.go
@@ -3,6 +3,7 @@ package api
 import (
 	"encoding/json"
 	"github.com/google/go-querystring/query"
+	"time"
 )
 
 func (client *ApiClient) GetAllTasks(params GetAllTasksParams) ([]Task, error) {
@@ -16,5 +17,47 @@ func (client *ApiClient) GetAllTasks(params GetAllTasksParams) ([]Task, error) {
 	if err != nil {
 		return nil, err
 	}
+	for i := range tasks {
+		tasks[i].CalculateUrgency()
+	}
 	return tasks, nil
+}
+
+func (task *Task) CalculateUrgency() {
+	if task.Done {
+		task.Urgency = 0
+		return
+	}
+	dueDateScore := task.getDueDateScore()
+	priorityScore := task.Priority
+	favoriteScore := 0
+	if task.IsFavorite {
+		favoriteScore = 1
+	}
+	task.Urgency = 1 + dueDateScore + priorityScore + favoriteScore
+}
+
+func (task *Task) getDueDateScore() int {
+	if task.DueDate == nil {
+		return 0
+	}
+	dueDays := int(task.DueDate.Sub(time.Now()).Hours() / 24)
+	switch {
+	case dueDays < 0:
+		return 6
+	case dueDays == 0:
+		return 5
+	case dueDays == 1:
+		return 4
+	case dueDays > 1 && dueDays <= 2:
+		return 3
+	case dueDays > 2 && dueDays <= 5:
+		return 2
+	case dueDays > 5 && dueDays <= 10:
+		return 1
+	case dueDays > 14:
+		return -1
+	default:
+		return 0
+	}
 }
