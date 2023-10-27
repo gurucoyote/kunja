250b9aefba0e11b2b543c904fd716df7be41a3b1
Guru Coyote <gc@testbild.tv>
2023-10-27 19:52:23 +0200

    Added a new command "new" to create a new task with a title and due date.

---
+++ cmd/task.go
@@ -0,0 +1,58 @@
+package cmd
+
+import (
+	"fmt"
+	"kunja/api"
+	"strings"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+var newCmd = &cobra.Command{
+	Use:   "new",
+	Short: "Create a new task",
+	Long:  `Create a new task using the provided title and due date.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		client := api.NewApiClient(BaseUrl, "")
+		_, err := client.Login(Username, Password, "")
+		if err != nil {
+			fmt.Println("Error logging in:", err)
+			return
+		}
+
+		title := strings.Join(args, " ")
+		due, _ := cmd.Flags().GetString("due")
+
+		var dueDate time.Time
+		if due != "" {
+			dueDate, err = time.Parse("2006-01-02", due)
+			if err != nil {
+				fmt.Println("Error parsing due date:", err)
+				return
+			}
+		}
+
+		task := api.Task{
+			Title:   title,
+			DueDate: dueDate,
+		}
+
+		projectId := 1
+		if Verbose {
+			client.Verbose = true
+		}
+		createdTask, err := client.CreateTask(projectId, task)
+		if err != nil {
+			fmt.Println("Error creating task:", err)
+			return
+		}
+
+		fmt.Println("Task created successfully:", createdTask.ID)
+	},
+}
+
+func init() {
+	newCmd.Flags().StringP("due", "d", "", "Due date for the task")
+	rootCmd.AddCommand(newCmd)
+}
